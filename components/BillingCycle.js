c059971111226f327051ed90d797d7d70c8cbbaf
Aakash Goel <aakash.lpin@gmail.com>
2017-10-24 12:23:56 +0530

    responsive

--- components/BillingCycle.js
+++ components/BillingCycle.js
@@ -1,24 +1,21 @@
 import { formatDate } from '../utils/utils';
 import TransactionsRenderer from './TransactionsRenderer';
 
-export default class BillingCycle extends React.Component {
-  render() {
-    const props = this.props;
-    return (
-      <div>
-        <div className="card">
-          <p className="label">
-            <span className="uc">Bill Cycle: </span>
-            {formatDate(props.bill_cyle_start)} &mdash; {formatDate(props.bill_cyle_end)}
-          </p>
+export default (props) => (
+  <div>
+    <div className="card">
+      <p className="label">
+        <span className="uc">Bill Cycle: </span>
+        {formatDate(props.bill_cyle_start)} &mdash; {formatDate(props.bill_cyle_end)}
+      </p>
 
-          <TransactionsRenderer
-            onClick={props.onClick}
-            transactions={props.transactions}
-          />
-        </div>
+      <TransactionsRenderer
+        onClick={props.onClick}
+        transactions={props.transactions}
+      />
+    </div>
 
-        <style jsx>{`
+    <style jsx>{`
           .card {
             background: #fff;
             padding: 2rem;
@@ -26,8 +23,13 @@ export default class BillingCycle extends React.Component {
             margin: 2rem auto;
             box-shadow: 0 0 5px 2px rgba(0,0,0,0.05);
           }
+          @media screen and (max-width: 768px) {
+            .card {
+              padding: 1rem;
+              width: 95%;
+              margin: 1rem auto;
+            }
+          }
         `}</style>
-      </div>
-    )
-  }
-}
+  </div>
+)
--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,8 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <title>Rival Corp Dashboard</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" />
           <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css' />
         </Head>
